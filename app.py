f03eae30c184fa11535dc8e648c434f11473aa85
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2024-11-19 10:30:05 +0100

    email test

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def _________GET_________(): pass
 @app.get("/")
 def view_index():
     name = "Tester"
+    email = "test"
     return render_template("view_index.html", name=name)
 
 if __name__ == "__main__":
