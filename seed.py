2ab9fe66f2b4dcdf4c12ce26b9008b79beaf1b56
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2024-11-20 14:40:04 +0100

    created tables and added 20 random users for each user role we have

--- seed.py
+++ seed.py
@@ -1,43 +1,279 @@
 import x
 import uuid
+import time
+import random
+from werkzeug.security import generate_password_hash
+from faker import Faker
+
+fake = Faker()
+
+from icecream import ic
+ic.configureOutput(prefix=f'***** | ', includeContext=True)
 
 db, cursor = x.db()
 
 def insert_user(user):
     q = f"""
         INSERT INTO users
-        VALUES (%s, %s ,%s, %s)
+        VALUES (%s, %s, %s, %s ,%s ,%s ,%s ,%s ,%s ,%s ,%s)
         """
     values = tuple(user.values())
     cursor.execute(q, values)
 try:
-    ##############################
+    ###############################
+    ######   Create users table
+    ###############################
+    cursor.execute("DROP TABLE IF EXISTS users_roles")
     cursor.execute("DROP TABLE IF EXISTS users")
-    
+    cursor.execute("DROP TABLE IF EXISTS roles")
     q = """
         CREATE TABLE users (
             user_pk CHAR(36),
             user_name VARCHAR(20) NOT NULL,
             user_last_name VARCHAR(20) NOT NULL,
-            user_email VARCHAR(50) NOT NULL,
+            user_email VARCHAR(100) NOT NULL UNIQUE,
+            user_password VARCHAR(255) NOT NULL,
+            user_created_at INTEGER UNSIGNED,
+            user_deleted_at INTEGER UNSIGNED,
+            user_blocked_at INTEGER UNSIGNED,
+            user_updated_at INTEGER UNSIGNED,
+            user_verified_at INTEGER UNSIGNED,
+            user_verification_key CHAR(36),
             PRIMARY KEY(user_pk)
         )
         """
     cursor.execute(q)
 
-    # Create User
+    ###############################
+    ######   Create roles table
+    ###############################
+    q = """
+        CREATE TABLE roles (
+            role_pk CHAR(36),
+            role_name VARCHAR(10) NOT NULL UNIQUE,
+            PRIMARY KEY(role_pk)
+        );
+        """
+    cursor.execute(q)
+
+    ##################################
+    ######   Create users_roles table
+    ##################################
+    q = """
+        CREATE TABLE users_roles (
+            user_role_user_fk CHAR(36),
+            user_role_role_fk CHAR(36),
+            PRIMARY KEY(user_role_user_fk, user_role_role_fk)
+        );
+        """
+    cursor.execute(q)
+    cursor.execute("ALTER TABLE users_roles ADD FOREIGN KEY (user_role_user_fk) REFERENCES users(user_pk) ON DELETE CASCADE ON UPDATE RESTRICT") 
+    cursor.execute("ALTER TABLE users_roles ADD FOREIGN KEY (user_role_role_fk) REFERENCES roles(role_pk) ON DELETE CASCADE ON UPDATE RESTRICT") 
+
+
+    ###############################
+    ######   Create roles
+    ###############################
+    q = f"""
+        INSERT INTO roles (role_pk, role_name)
+        VALUES ("{x.ADMIN_ROLE_PK}", "admin"), ("{x.CUSTOMER_ROLE_PK}", "customer"),
+        ("{x.PARTNER_ROLE_PK}", "partner"), ("{x.RESTAURANT_ROLE_PK}", "restaurant")
+        """
+    cursor.execute(q)
+
+    ###############################
+    ######   Create Admin user
+    ###############################
     user_pk = str(uuid.uuid4())
     user = {
         "user_pk" : user_pk,
-        "user_name" : "Santiago",
-        "user_last_name" : "Donoso",
-        "user_email": "a@a.com"
+        "user_name" : "John",
+        "user_last_name" : "Admin",
+        "user_email" : "admin@fulldemo.com",
+        "user_password" : generate_password_hash("password"),
+        "user_created_at" : int(time.time()),
+        "user_deleted_at" : 0,
+        "user_blocked_at" : 0,
+        "user_updated_at" : 0,
+        "user_verified_at" : int(time.time()),
+        "user_verification_key" : str(uuid.uuid4())
     }
     insert_user(user)
+    # Assign role to admin user
+    q = f"""
+        INSERT INTO users_roles (user_role_user_fk, user_role_role_fk) VALUES ("{user_pk}",
+        "{x.ADMIN_ROLE_PK}")
+        """
+    cursor.execute(q)
+
+    ###############################
+    ######   Create customer user
+    ###############################
+    user_pk = "4218788d-03b7-4812-bd7d-31c8859e92d8"
+    user = {
+        "user_pk" : user_pk,
+        "user_name" : "John",
+        "user_last_name" : "Customer",
+        "user_email" : "customer@fulldemo.com",
+        "user_password" : generate_password_hash("password"),
+        "user_created_at" : int(time.time()),
+        "user_deleted_at" : 0,
+        "user_blocked_at" : 0,
+        "user_updated_at" : 0,
+        "user_verified_at" : int(time.time()),
+        "user_verification_key" : str(uuid.uuid4())
+    }
+    insert_user(user)
+    # Assign role to customer user
+    q = f"""
+        INSERT INTO users_roles (user_role_user_fk, user_role_role_fk) VALUES ("{user_pk}",
+        "{x.CUSTOMER_ROLE_PK}")
+        """
+    cursor.execute(q)
+
+    ###############################
+    ######   Create partner user
+    ###############################
+    user_pk = str(uuid.uuid4())
+    user = {
+        "user_pk" : user_pk,
+        "user_name" : "John",
+        "user_last_name" : "Partner",
+        "user_email" : "partner@fulldemo.com",
+        "user_password" : generate_password_hash("password"),
+        "user_created_at" : int(time.time()),
+        "user_deleted_at" : 0,
+        "user_blocked_at" : 0,
+        "user_updated_at" : 0,
+        "user_verified_at" : int(time.time()),
+        "user_verification_key" : str(uuid.uuid4())
+    }
+    insert_user(user)
+    # Assign role to partner user
+    q = f"""
+        INSERT INTO users_roles (user_role_user_fk, user_role_role_fk) VALUES ("{user_pk}",
+        "{x.PARTNER_ROLE_PK}")
+        """
+    cursor.execute(q)
+
+    #################################
+    ######   Create restaurant user
+    #################################
+    user_pk = str(uuid.uuid4())
+    user = {
+        "user_pk" : user_pk,
+        "user_name" : "John",
+        "user_last_name" : "Restaurant",
+        "user_email" : "restaurant@fulldemo.com",
+        "user_password" : generate_password_hash("password"),
+        "user_created_at" : int(time.time()),
+        "user_deleted_at" : 0,
+        "user_blocked_at" : 0,
+        "user_updated_at" : 0,
+        "user_verified_at" : int(time.time()),
+        "user_verification_key" : str(uuid.uuid4())
+    }
+    insert_user(user)
+    # Assign role to restaurant user
+    q = f"""
+        INSERT INTO users_roles (user_role_user_fk, user_role_role_fk) VALUES ("{user_pk}",
+        "{x.RESTAURANT_ROLE_PK}")
+        """
+    cursor.execute(q)
+
+
+    ###############################
+    ######   Create 20 customers
+    ###############################
+    domains = ["example.com", "testsite.org", "mydomain.net", "website.co", "fakemail.io", "gmail.com", "hotmail.com"]
+    user_password = hashed_password = generate_password_hash("password")
+    for _ in range(20):
+        user_pk = str(uuid.uuid4())
+        user_verified_at = random.choice([0,int(time.time())])
+        user = {
+            "user_pk" : user_pk,
+            "user_name" : fake.first_name(),
+            "user_last_name" : fake.last_name(),
+            "user_email" : fake.unique.user_name() + "@" + random.choice(domains),
+            "user_password" : user_password,
+            # user_password = hashed_password = generate_password_hash(fake.password(length=20))
+            "user_created_at" : int(time.time()),
+            "user_deleted_at" : 0,
+            "user_blocked_at" : 0,
+            "user_updated_at" : 0,
+            "user_verified_at" : user_verified_at,
+            "user_verification_key" : str(uuid.uuid4())
+        }
+        insert_user(user)
+        # assign role to customer users
+        cursor.execute("""INSERT INTO users_roles (
+            user_role_user_fk,
+            user_role_role_fk)
+            VALUES (%s, %s)""", (user_pk, x.CUSTOMER_ROLE_PK))
+        
+    ###############################
+    ######   Create 20 partners
+    ###############################
+    user_password = hashed_password = generate_password_hash("password")
+    for _ in range(20):
+        user_pk = str(uuid.uuid4())
+        user_verified_at = random.choice([0,int(time.time())])
+        user = {
+            "user_pk" : user_pk,
+            "user_name" : fake.first_name(),
+            "user_last_name" : fake.last_name(),
+            "user_email" : fake.unique.email(),
+            "user_password" : user_password,
+            "user_created_at" : int(time.time()),
+            "user_deleted_at" : 0,
+            "user_blocked_at" : 0,
+            "user_updated_at" : 0,
+            "user_verified_at" : user_verified_at,
+            "user_verification_key" : str(uuid.uuid4())
+        }
+        insert_user(user)
+        # assign role to partner users
+        cursor.execute("""
+        INSERT INTO users_roles (
+            user_role_user_fk,
+            user_role_role_fk)
+            VALUES (%s, %s)
+        """, (user_pk, x.PARTNER_ROLE_PK))
+
+
+    ###############################
+    ######   Create 20 restaurants
+    ###############################
+    user_password = hashed_password = generate_password_hash("password")
+    for _ in range(20):
+        user_pk = str(uuid.uuid4())
+        user_verified_at = random.choice([0,int(time.time())])
+        user = {
+            "user_pk" : user_pk,
+            "user_name" : fake.first_name(),
+            "user_last_name" : "",
+            "user_email" : fake.unique.email(),
+            "user_password" : user_password,
+            "user_created_at" : int(time.time()),
+            "user_deleted_at" : 0,
+            "user_blocked_at" : 0,
+            "user_updated_at" : 0,
+            "user_verified_at" : user_verified_at,
+            "user_verification_key" : str(uuid.uuid4())
+        }
+        insert_user(user)
+        # assign role to restaurant users
+        cursor.execute("""
+        INSERT INTO users_roles (
+            user_role_user_fk,
+            user_role_role_fk)
+            VALUES (%s, %s)
+        """, (user_pk, x.RESTAURANT_ROLE_PK))
 
     db.commit()
 
 except Exception as ex:
+    ic(ex)
     if "db" in locals(): db.rollback()
 
 finally:
