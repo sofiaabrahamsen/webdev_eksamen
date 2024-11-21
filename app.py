b902aa07a7b884f10c6b4695a6ea1dab1ceaa9aa
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2024-11-21 16:45:00 +0100

    backend seems to work for login. the css files moved to static folder so flask can find them and use them. also added som js and css so we can use mixhtml (not sure if we need to do more with it)

--- app.py
+++ app.py
@@ -1,17 +1,98 @@
-from flask import Flask, render_template, app
+from flask import Flask, render_template, app, session, redirect, url_for, request
+from flask_session import Session
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 import x
 
-app = Flask(__name__)
+from icecream import ic
+ic.configureOutput(prefix=f'***** | ', includeContext=True)
 
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+###################################
+###################################
 def _________GET_________(): pass
+###################################
+###################################
 
 ##############################
+# Login / landingpage
+##############################
 @app.get("/")
-def view_index():
-    name = "Tester"
-    email = "test"
-    number = 1
-    return render_template("view_index.html", name=name)
+@x.no_cache
+def view_login():
+    ic(session)
+    if session.get("user"):
+        if len(session.get("user").get("roles")) > 1:
+            return redirect(url_for("view_choose_role"))
+        if "admin" in session.get("user").get("roles"):
+            return redirect(url_for("view_admin"))
+        if "customer" in session.get("user").get("roles"):
+            return redirect(url_for("view_customer"))
+        if "partner" in session.get("user").get("roles"):
+            return redirect(url_for("view_partner"))
+    return render_template("view_login.html", x=x, title="Login", message=request.args.get("message", ""))
+
+###################################
+###################################
+def _________POST_________(): pass
+###################################
+###################################
+
+##############################
+# Login /landingpage
+##############################
+@app.post("/")
+def login():
+    try:
+        user_email = x.validate_user_email()
+        user_password = x.validate_user_password()
+        db, cursor = x.db()
+        q = """ SELECT * FROM users
+                JOIN users_roles
+                ON user_pk = user_role_user_fk
+                JOIN roles
+                ON role_pk = user_role_role_fk
+                WHERE user_email = %s"""
+        cursor.execute(q, (user_email,))
+        rows = cursor.fetchall()
+        if not rows:
+            toast = render_template("___toast.html", message="user not registered")
+            return f"""<template mix-target="#toast">{toast}</template>""", 400
+        if not check_password_hash(rows[0]["user_password"], user_password):
+            toast = render_template("___toast.html", message="invalid credentials")
+            return f"""<template mix-target="#toast">{toast}</template>""", 401
+        roles = []
+        for row in rows:
+            roles.append(row["role_name"])
+        user = {
+            "user_pk": rows[0]["user_pk"],
+            "user_name": rows[0]["user_name"],
+            "user_last_name": rows[0]["user_last_name"],
+            "user_email": rows[0]["user_email"],
+            "roles": roles
+        }
+        ic(user)
+        session["user"] = user
+        if len(roles) == 1:
+            return f"""<template mix-redirect="/{roles[0]}"></template>"""
+        return f"""<template mix-redirect="/choose-role"></template>"""
+    except Exception as ex:
+        ic(ex)
+        if "db" in locals(): db.rollback()
+        if isinstance(ex, x.CustomException):
+            toast = render_template("___toast.html", message=ex.message)
+            return f"""<template mix-target="#toast" mix-bottom>{toast}</template>""", ex.code    
+        if isinstance(ex, x.mysql.connector.Error):
+            ic(ex)
+            return "<template>System upgrading</template>", 500
+        return "<template>System under maintenance</template>", 500
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- x.py
+++ x.py
@@ -1,9 +1,24 @@
+from flask import make_response, request
+from functools import wraps
 import mysql.connector
+import re
+
+from icecream import ic
+ic.configureOutput(prefix=f'***** | ', includeContext=True)
 
 ADMIN_ROLE_PK = "16fd2706-8baf-433b-82eb-8c7fada847da"
 CUSTOMER_ROLE_PK = "c56a4180-65aa-42ec-a945-5fd21dec0538"
 PARTNER_ROLE_PK = "f47ac10b-58cc-4372-a567-0e02b2c3d479"
 RESTAURANT_ROLE_PK = "9f8c8d22-5a67-4b6c-89d7-58f8b8cb4e15"
+
+class CustomException(Exception):
+    def __init__(self, message, code):
+        super().__init__(message)  # Initialize the base class with the message
+        self.message = message  # Store additional information (e.g., error code)
+        self.code = code  # Store additional information (e.g., error code)
+
+def raise_custom_exception(error, status_code):
+    raise CustomException(error, status_code)
 
 ##############################
 def db():
@@ -15,3 +30,49 @@ def db():
     )
     cursor = db.cursor(dictionary=True)
     return db, cursor
+
+##############################
+def no_cache(view):
+    @wraps(view)
+    def no_cache_view(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
+    return no_cache_view
+
+##############################
+def allow_origin(origin="*"):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            # Call the wrapped function
+            response = make_response(f(*args, **kwargs))
+            # Add Access-Control-Allow-Origin header to the response
+            response.headers["Access-Control-Allow-Origin"] = origin
+            # Optionally allow other methods and headers for full CORS support
+            response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS, PUT, DELETE"
+            response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+            return response
+        return decorated_function
+    return decorator
+
+
+##############################
+REGEX_EMAIL = "^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"
+def validate_user_email():
+    error = "email invalid"
+    user_email = request.form.get("user_email", "").strip()
+    if not re.match(REGEX_EMAIL, user_email): raise_custom_exception(error, 400)
+    return user_email
+
+##############################
+USER_PASSWORD_MIN = 8
+USER_PASSWORD_MAX = 50
+REGEX_USER_PASSWORD = f"^.{{{USER_PASSWORD_MIN},{USER_PASSWORD_MAX}}}$"
+def validate_user_password():
+    error = f"password {USER_PASSWORD_MIN} to {USER_PASSWORD_MAX} characters"
+    user_password = request.form.get("user_password", "").strip()
+    if not re.match(REGEX_USER_PASSWORD, user_password): raise_custom_exception(error, 400)
+    return user_password
